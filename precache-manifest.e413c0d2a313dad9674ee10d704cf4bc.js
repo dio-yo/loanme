ae8eff8db1eab88550bde1ec4d6051cf0ab177f1
Dhrubesh Deb Sharma <dhrubesh97@gmail.com>
2020-07-11 14:45:49 +0530

    Updates

---
+++ precache-manifest.e413c0d2a313dad9674ee10d704cf4bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2ac6a236fc654fcb2e97067c42e1fb8",
+    "url": "/loanme.github.io/index.html"
+  },
+  {
+    "revision": "a86110c0def99a65be8a",
+    "url": "/loanme.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "393170496b4b582eeed5",
+    "url": "/loanme.github.io/static/js/2.7270e7c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/loanme.github.io/static/js/2.7270e7c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a86110c0def99a65be8a",
+    "url": "/loanme.github.io/static/js/main.8bdd3508.chunk.js"
+  },
+  {
+    "revision": "6a0f1d9efd3a7e27cfb2",
+    "url": "/loanme.github.io/static/js/runtime-main.e65bf022.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/loanme.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
